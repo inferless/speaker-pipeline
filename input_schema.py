5eb08f2d74843a2e0de1d44ecaabc5f9dff5c371
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-02-05 23:46:42 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,52 @@
+INPUT_SCHEMA = {
+  "transcription_file": 
+    {
+      "datatype": "BYTES",
+      "shape": [
+        1
+      ],
+      "example": [
+        "https://inferless-public.s3.amazonaws.com/sample.txt"
+      ]
+    },
+"alpha" :
+    {
+      "datatype": "FP32",
+      "shape": [
+        1
+      ],
+      "example": [
+        0.123
+      ]
+    },
+"beta" : 
+    {
+      "datatype": "FP32",
+      "shape": [
+        1
+      ],
+      "example": [
+        0.456
+      ]
+    },
+"speed" : 
+    {
+      "datatype": "FP32",
+      "shape": [
+        1
+      ],
+      "data": [
+        1.25
+      ]
+    },
+"target_lang" : 
+    {
+      "datatype": "BYTES",
+      "shape": [
+        1
+      ],
+      "data": [
+        "fr"
+      ]
+    }
+}
